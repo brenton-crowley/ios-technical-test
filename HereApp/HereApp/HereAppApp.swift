bddb04456c886883cec04295caf413503290a433
brenton-crowley <brenton.crowley@gmail.com>
2023-05-04 08:11:25 +1000

    initial commit to start project: https://github.com/brenton-crowley/ios-technical-test

---
+++ HereApp/HereApp/HereAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HereAppApp.swift
+//  HereApp
+//
+//  Created by Brent Crowley on 4/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct HereAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
